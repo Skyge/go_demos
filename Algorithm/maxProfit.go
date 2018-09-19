c8c8315d949bd0bf66e41a5afe05723313e7dc76
李天奇 <litianqi@ofbank.com>
2018-09-19 09:32:28 +0800

    Get max profit.

---
+++ Algorithm/maxProfit.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{7, 1, 5, 6, 3, 6, 4}
+	output := maxProfit(nums)
+	fmt.Println(output)
+}
+
+/**
+* Say you have an array for which the ith element is the price of a given stock on day i.
+* Design an algorithm to find the maximum profit.
+* You may complete as many transactions as you like：
+* (i.e., buy one and sell one share of the stock multiple times).
+* Note: You may not engage in multiple transactions at the same time：
+* (i.e., you must sell the stock before you buy again).
+ */
+func maxProfit(nums []int) int {
+	lens := len(nums)
+	profit := 0
+	for i := 0; i < lens-1; i++ {
+		if nums[i] < nums[i+1] {
+			profit += nums[i+1] - nums[i]
+		}
+	}
+	return profit
+}
--- Algorithm/removeDuplicate.go
+++ Algorithm/removeDuplicate.go
@@ -2,29 +2,27 @@ package main
 
 import "fmt"
 
-/*	Given a sorted array nums,
-	remove the duplicates in-place such that each element appear only once and return the new length.
-
-	Do not allocate extra space for another array,
-	you must do this by modifying the input array in-place with O(1) extra memory.
- */
-func main(){
-	nums := []int{0,0,1,1,1,2,2,3,3,4}
+func main() {
+	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
 	numsLen := removeDuplicates(nums)
 	fmt.Println(numsLen)
 	fmt.Println(nums)
 }
 
+/* *
+* Given a sorted array nums,
+* remove the duplicates in-place such that each element appear only once and return the new length.
+* Do not allocate extra space for another array,
+* you must do this by modifying the input array in-place with O(1) extra memory.
+ */
 func removeDuplicates(nums []int) int {
 	number := 0
 	lens := len(nums)
-	for i := 0; i < lens ; i++ {
-	   if nums[i] != nums[number] {
-		  number++
-		  nums[number] = nums[i]
-	   }
+	for i := 0; i < lens; i++ {
+		if nums[i] != nums[number] {
+			number++
+			nums[number] = nums[i]
+		}
 	}
-	return number+1
+	return number + 1
 }
-
-
