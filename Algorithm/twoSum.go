8016f52a1fd3a4bd1a0922065c35244a6e05193e
Skyge <18844062006@163.com>
2018-09-25 22:41:41 +0800

    Find two sum.

---
+++ Algorithm/twoSum.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	f "fmt"
+)
+
+func main() {
+	nums := []int{2, 8, 11, 15, 7}
+	target := 9
+	output := twoSum(nums, target)
+	f.Println(output)
+}
+
+/* *
+* Given an array of integers,
+* return indices of the two numbers such that they add up to a specific target.
+* You may assume that each input would have exactly one solution,
+* and you may not use the same element twice.
+ */
+func twoSum(nums []int, target int) []int  {
+	result := make([]int, 2)
+	temp := make(map[int]int)
+	for i, num :=range nums {
+		if expected, exited := temp[target - num]; exited {
+			result[0] = expected
+			result[1] = i
+			return result
+		}
+		temp[num] = i
+	}
+	return result
+}
