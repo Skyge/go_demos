38b2aea63efc51f840321c1a1cc827f4f385577e
Skyge <18844062006@163.com>
2018-10-16 22:55:12 +0800

    Achieve rotate array in another way

--- Algorithm/rotateArray.go
+++ Algorithm/rotateArray.go
@@ -5,8 +5,10 @@ import "fmt"
 func main() {
 	nums := []int{1, 2, 3, 4, 5, 6, 7}
 	k := 3
-	output := rotate(nums, k)
-	fmt.Println(output)
+	//output := rotate(nums, k)
+	//fmt.Println(output)
+	rotate2(nums, k)
+	fmt.Println(nums)
 }
 
 /**
@@ -18,4 +20,31 @@ func rotate(nums []int, k int) []int {
 		copy(nums, append(nums[len(nums)-k:], nums[:len(nums)-k]...))
 	}
 	return nums
+}
+
+// method2
+func rotate2(nums []int, k int) {
+	// 假定 k >= 0
+
+	n := len(nums)
+
+	if k > n {
+		k %= n
+	}
+	if k == 0 || k == n {
+		return
+	}
+
+	reverse(nums, 0, n-1)
+	reverse(nums, 0, k-1)
+	reverse(nums, k, n-1)
+
+}
+
+func reverse(nums []int, i, j int) {
+	for i < j {
+		nums[i], nums[j] = nums[j], nums[i]
+		i++
+		j--
+	}
 }
