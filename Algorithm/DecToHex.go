c567565b210e3bf92cb650f1c7c8847143db90ce
Skyge <18844062006@163.com>
2018-12-29 23:13:19 +0800

    A Decimal to Hexadecimal

---
+++ Algorithm/DecToHex.go
@@ -0,0 +1,28 @@
+package Algorithm
+
+import (
+	"fmt"
+	"log"
+)
+
+func DecHex(n int64) string {
+	if n < 0 {
+		log.Println("Decimal to hexadecimal error: the argument must be greater than zero.")
+		return ""
+	}
+	if n == 0 {
+		return "0"
+	}
+	hex := map[int64]int64{10: 65, 11: 66, 12: 67, 13: 68, 14: 69, 15: 70}
+	s := ""
+	for q := n; q > 0; q = q / 16 {
+		m := q % 16
+		if m > 9 && m < 16 {
+			m = hex[m]
+			s = fmt.Sprintf("%v%v", string(m), s)
+			continue
+		}
+		s = fmt.Sprintf("%v%v", m, s)
+	}
+	return s
+}
