402f765d8b83d1b3113322cfb6ec29e96701bdb2
Skyge <18844062006@163.com>
2019-01-03 23:48:34 +0800

    A Merge two lists

---
+++ Algorithm/LinkedList/MergeTwoLists.go
@@ -0,0 +1,38 @@
+package LinkedList
+
+/**
+Merge two sorted linked lists and return it as a new list.
+The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+	Input: 1->2->4, 1->3->4
+	Output: 1->1->2->3->4->4
+ */
+
+/**
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+ */
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	var newList  = &ListNode{}
+	var out = newList
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			newList.Next = l1
+			l1 = l1.Next
+		}else{
+			newList.Next = l2
+			l2 = l2.Next
+		}
+		newList = newList.Next
+	}
+	if l1 != nil {
+		newList.Next = l1
+	}else if l2 != nil {
+		newList.Next = l2
+	}
+
+	return out.Next
+}
