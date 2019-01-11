5c75183c0aa4199af47ab257c5a9a3c184555207
Skyge <18844062006@163.com>
2019-01-11 09:11:09 +0800

    A Max depth of a binary tree.

---
+++ Algorithm/Trees/MaxDepth.go
@@ -0,0 +1,36 @@
+package Trees
+
+/**
+Given a binary tree, find its maximum depth.
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+	Given binary tree [3,9,20,null,null,15,7],
+
+		3
+	   / \
+	  9  20
+		/  \
+	   15   7
+return its depth = 3.
+ */
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root != nil {
+		leftDepth := maxDepth(root.Left)
+		rightDepth := maxDepth(root.Right)
+		if leftDepth > rightDepth {
+			return 1+leftDepth
+		}
+		return 1+rightDepth
+	}
+	return 0
+}
