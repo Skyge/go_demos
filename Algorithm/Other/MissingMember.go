963bdb6c8e55abe76fdfa8fede50ebbc3ae693a2
skyge <18844062006@163.com>
2019-03-15 21:21:17 +0800

    U Optimize code.

--- Algorithm/Other/MissingMember.go
+++ Algorithm/Other/MissingMember.go
@@ -15,16 +15,28 @@ Note:
 Your algorithm should run in linear runtime complexity.
 Could you implement it using only constant extra space complexity?
 */
-func missingNumber(nums []int) (ret int) {
-	n := len(nums)
-	if n == 0 {
-		return
+// method1:
+//func missingNumber(nums []int) (ret int) {
+//	n := len(nums)
+//	if n == 0 {
+//		return
+//	}
+//
+//	sum := 0
+//	for _, v := range nums {
+//		sum += v
+//	}
+//
+//	return (n+1)*n/2 - sum // (0+n)*(n+1)/2  = (n+1)*n/2
+//}
+// method2
+func missingNumber(nums []int) int {
+	// n^m^n = m  so (0^1^2^3...^n)  ^ (0^1^3...^n) = 2(missing number)
+	res := len(nums)
+	for i := 0; i < len(nums); i++ {
+		res ^= nums[i]
+		res ^= i
 	}
 
-	sum := 0
-	for _, v := range nums {
-		sum += v
-	}
-
-	return (n+1)*n/2 - sum // (0+n)*(n+1)/2  = (n+1)*n/2
+	return res
 }
